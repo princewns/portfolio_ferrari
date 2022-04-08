1f18e843eaa86d58794b9e602a33614d634588f4
princewns <princewns@gmail.com>
2022-04-08 17:50:06 +0900

    220408

---
+++ components/intro_component.js
@@ -0,0 +1,67 @@
+Vue.component('intro-component',{
+    template : 
+    `
+    <div>
+        <div class="section02">
+            <div class="section02Intro">
+                <h1 data-aos="fade-up" data-aos-duration="1000">1947</h1>
+                <p data-aos="fade-up" data-aos-duration="2000">전설의 시작</p>
+                <ul data-aos="fade-up" data-aos-duration="3000">
+                    <li>1924년 스쿠데리아 페라리 레이싱 팀으로 시작된</li>
+                    <li>페라리는 창립자 엔조 페라리(Enzo Ferrari)의 레이싱 팀 경험을 살려</li>
+                    <li>1947년 이탈리아 모데나 공장에서 최초의 모델 '125S' 모델을 출시합니다</li>
+                </ul>
+            </div>
+            <div class="section02Img">
+                <img src="./img/125s.jpg" alt="ferrati_125s" 
+                data-aos="flip-up" data-aos-duration="3000">
+            </div>
+        </div>
+        <div class="section03">
+            <div class="section03Intro">
+                <h1 data-aos="fade-up" data-aos-duration="1000">1947</h1>
+                <p data-aos="fade-up" data-aos-duration="2000">전설의 시작</p>
+                <ul data-aos="fade-up" data-aos-duration="3000">
+                    <li>1924년 스쿠데리아 페라리 레이싱 팀으로 시작된</li>
+                    <li>페라리는 창립자 엔조 페라리(Enzo Ferrari)의 레이싱 팀 경험을 살려</li>
+                    <li>1947년 이탈리아 모데나 공장에서 최초의 모델 '125S' 모델을 출시합니다</li>
+                </ul>
+            </div>
+            <div class="section03Img">
+                <img src="./img/125s.jpg" alt="ferrati_125s" 
+                data-aos="flip-up" data-aos-duration="3000">
+            </div>
+        </div>
+        <div class="section04">
+            <div class="section04Intro">
+                <h1 data-aos="fade-up" data-aos-duration="1000">1947</h1>
+                <p data-aos="fade-up" data-aos-duration="2000">전설의 시작</p>
+                <ul data-aos="fade-up" data-aos-duration="3000">
+                    <li>1924년 스쿠데리아 페라리 레이싱 팀으로 시작된</li>
+                    <li>페라리는 창립자 엔조 페라리(Enzo Ferrari)의 레이싱 팀 경험을 살려</li>
+                    <li>1947년 이탈리아 모데나 공장에서 최초의 모델 '125S' 모델을 출시합니다</li>
+                </ul>
+            </div>
+            <div class="section04Img">
+                <img src="./img/125s.jpg" alt="ferrati_125s" 
+                data-aos="flip-up" data-aos-duration="3000">
+            </div>
+        </div>
+        <div class="section05">
+            <div class="section05Intro">
+                <h1 data-aos="fade-up" data-aos-duration="1000">1947</h1>
+                <p data-aos="fade-up" data-aos-duration="2000">전설의 시작</p>
+                <ul data-aos="fade-up" data-aos-duration="3000">
+                    <li>1924년 스쿠데리아 페라리 레이싱 팀으로 시작된</li>
+                    <li>페라리는 창립자 엔조 페라리(Enzo Ferrari)의 레이싱 팀 경험을 살려</li>
+                    <li>1947년 이탈리아 모데나 공장에서 최초의 모델 '125S' 모델을 출시합니다</li>
+                </ul>
+            </div>
+            <div class="section05Img">
+                <img src="./img/125s.jpg" alt="ferrati_125s" 
+                data-aos="flip-up" data-aos-duration="3000">
+            </div>
+        </div>
+    </div>
+    `,
+})
---
+++ components/video_component.js
@@ -0,0 +1,19 @@
+Vue.component('video-component',{
+    template : 
+    `
+      <div class="section01" data-aos="fade-up" data-aos-duration="3000">
+          <video muted autoplay loop>
+            <source src="./video/ferrari.mp4" type="video/mp4">
+            <strong>Your browser does not support the video tag.</strong>
+          </video>
+        <div class="title-text">
+          <h1>FERRARI : Spirit of Italian</h1>
+        </div>
+        <div class="title-arrow">
+          <v-icon class="white--text" x-large="true"
+          transition="scroll-y-transition"
+          loop>mdi-chevron-down</v-icon>
+        </div>
+      </div>
+    `,
+})
